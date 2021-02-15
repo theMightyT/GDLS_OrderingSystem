5e58ba4ed895593ed53708d9ee7a2d2d39ba0358
tvanrys <tvanrys@fanshaweonline.ca>
2021-02-15 16:26:37 -0500

    adding cron job

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const cron = require('node-cron');
 
 const port = process.env.PORT || 3000;
 
@@ -15,3 +16,13 @@ server.use("/", require('./routes/index'));
 server.listen(port, () => {
     console.log(`app is running on ${port}`);
 })
+
+// schedule cron job
+cron.schedule('3 5 * * *', () => {
+    console.log('runs every day at 5AM');
+
+    // import and parse CSV file here, compare against existing data
+    // the CSV reader / writer live in the route file, so this would likely have to  move
+    // cron job is just here for reference
+    console.log('parse .csv, update datastore');
+});
